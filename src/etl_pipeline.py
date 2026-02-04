53eb2895f6bf727cf5791712282dc4ddfc7606dc
Ghoui <abdulalian201@gmail.com>
2026-02-04 13:16:54 +0000

    refactor: removed not neeed if statement

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -1,29 +1,19 @@
 import json
 import logging
-import os
-from pathlib import Path
 
 import pandas as pd
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
-
-# DIRECTORIES
-BASE_DIRECTORY = Path(__file__).resolve().parent
-DATA_DIRECTORY = BASE_DIRECTORY / "data"
-RAW_DATA_DIRECTORY = DATA_DIRECTORY / "raw"
-# URLS
-FAKE_STORE_API_URL = os.getenv("FAKE_STORE_API_URL")
+from config import Config
 
 
 def extract_products():
     """This function extracts products data from the Fake Store API."""
-    if not FAKE_STORE_API_URL:
-        logging.info(FAKE_STORE_API_URL)
+    if not Config.FAKE_STORE_API_URL:
+        logging.info(Config.FAKE_STORE_API_URL)
         raise ValueError("FAKE_STORE_API_URL is not set")
     try:
-        response = requests.get(FAKE_STORE_API_URL + "/products")
+        response = requests.get(Config.FAKE_STORE_API_URL + "/products")
         response.raise_for_status()
         data = response.json()
         logging.info("Data extracted successfully")
@@ -37,10 +27,7 @@ def save_raw_data(data):
     if not data:
         raise ValueError("No data to save")
 
-    if not RAW_DATA_DIRECTORY.exists():
-        RAW_DATA_DIRECTORY.mkdir(parents=True)
-
-    file_path = RAW_DATA_DIRECTORY / "products.json"
+    file_path = Config.RAW_DATA_DIRECTORY / "products.json"
     if file_path.exists():
         logging.info(
             f"Raw data already exists at {file_path}. Please delete it before saving new data."
@@ -60,7 +47,8 @@ def transform_data(data):
     df = df.drop(columns=["rating"], axis=1)
 
     # Renaming columns
-    df = df.rename(columns={"image": "image_url"})
+    df = df.rename(columns={"image": "image_url"})  # Being more specific
+
     return df
 
 
